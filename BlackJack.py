17adfb791dd4f67822cfdf716e609dcce3bd44aa
Esteban Montesinos <32073048+EstebanMontesinos@users.noreply.github.com>
2019-02-21 17:09:30 -0700

    Upgraded Nav bar using bootstrap configs

--- BlackJack.py
+++ BlackJack.py
@@ -9,5 +9,15 @@ def home():
     return render_template("index.html",title="Homepage")
 
 
+@app.route('/account')
+def account_page():
+    return render_template("Account_Page.html",title="Account")
+
+
+@app.route('/about')
+def about_page():
+    return render_template("About_page.html",title="About")
+
+
 if __name__ == '__main__':
     app.run()

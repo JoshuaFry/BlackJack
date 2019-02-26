c56ab6106b41a15b7d0f468365cf57e4f10337e9
admin <joshua.fry@western.edu>
2019-02-26 10:18:32 -0700

    pyrbase init

--- BlackJack.py
+++ BlackJack.py
@@ -1,13 +1,53 @@
 from flask import Flask
 from flask import Flask,request,render_template
+import uuid
+import pyrebase
+
+src = "https://www.gstatic.com/firebasejs/5.8.3/firebase.js"
+
+config = {
+    "apiKey": "AIzaSyAEGhM6e4oYckUrj-25itp6IbFgYfffkH8",
+    "authDomain": "blackjack-22b3a.firebaseapp.com",
+    "databaseURL": "https://blackjack-22b3a.firebaseio.com",
+    "projectId": "blackjack-22b3a",
+    "storageBucket": "blackjack-22b3a.appspot.com",
+    "messagingSenderId": "509859351763"
+}
+
+firebase = pyrebase.initialize_app(config)
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
 
+def test_data():
+    table = uuid.uuid4()
+
+    name = "testName"
+    table = {
+        "name" : name,
+        "seats" : {
+            1 : "empty",
+            2 : "empty",
+            3: "empty",
+            4: "empty",
+            5: "empty",
+            6: "empty"
+            }
+         }
+    return table
+
+
 if __name__ == '__main__':
+    auth = firebase.auth()
+    db = firebase.database()
+    user = auth.sign_in_with_email_and_password("joshua.fry@western.edu", "pass2019")
+    user = auth.refresh(user['refreshToken'])
+
+    # results = db.child("table").push(test_data(), user['idToken'])
+
     app.run()
+
---
+++ scripts/db_config.js
@@ -0,0 +1,11 @@
+src="https://www.gstatic.com/firebasejs/5.8.3/firebase.js"
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAEGhM6e4oYckUrj-25itp6IbFgYfffkH8",
+    authDomain: "blackjack-22b3a.firebaseapp.com",
+    databaseURL: "https://blackjack-22b3a.firebaseio.com",
+    projectId: "blackjack-22b3a",
+    storageBucket: "blackjack-22b3a.appspot.com",
+    messagingSenderId: "509859351763"
+  };
+  firebase.initializeApp(config);

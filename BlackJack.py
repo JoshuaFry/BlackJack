5ecd7e7a0dac2f1d31f8dd210ef3dbb58dae45ef
admin <joshua.fry@western.edu>
2019-02-27 13:42:23 -0700

    Firebase login/register

--- BlackJack.py
+++ BlackJack.py
@@ -16,11 +16,13 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+
     return render_template("index.html",title="Homepage")
 
 
@@ -32,6 +34,28 @@ def account_page():
 @app.route('/about')
 def about_page():
     return render_template("About_page.html",title="About")
+
+
+@app.route('/register', methods = ['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+    auth.create_user_with_email_and_password(email, password)
+    user = auth.sign_in_with_email_and_password(email,password)
+    user = auth.refresh(user['refreshToken'])
+
+    return render_template("index.html",title="Homepage")
+
+
+@app.route('/signin', methods = ['POST'])
+def signin_user():
+    email = request.form['email']
+    password = request.form['password']
+    user = auth.sign_in_with_email_and_password(email,password)
+    user = auth.refresh(user['refreshToken'])
+    # print(auth.current_user)
+
+    return render_template("index.html",title="Homepage")
 
 
 def test_data():
@@ -53,10 +77,10 @@ def test_data():
 
 
 if __name__ == '__main__':
-    auth = firebase.auth()
-    db = firebase.database()
-    user = auth.sign_in_with_email_and_password("joshua.fry@western.edu", "pass2019")
-    user = auth.refresh(user['refreshToken'])
+    # auth = firebase.auth()
+    # db = firebase.database()
+    # user = auth.sign_in_with_email_and_password("joshua.fry@western.edu", "pass2019")
+    # user = auth.refresh(user['refreshToken'])
 
     # results = db.child("table").push(test_data(), user['idToken'])
 
